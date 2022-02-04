3c850b3b93c74266d34abeeeb091603a7a02baa6
abiramitoronto <nagaabirami.toronto@gmail.com>
2022-02-04 12:53:46 -0500

    Updated Admin Dashboard Counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  ENV["HTTP_BASIC_PASSWORD"]
   def show
+    @product_count = Product.count
+    @category_count = Category.count
+
   end
 end
